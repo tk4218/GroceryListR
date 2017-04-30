0dc25d694ae0f59f68949e2af259f5966a553f11
tk4218 <tk4218@email.arizona.edu>
2017-04-30 11:09:41 -0600

    Minor change to verify push is working.

--- app/src/main/java/com/tk4218/grocerylistr/Fragments/CalendarFragment.java
+++ app/src/main/java/com/tk4218/grocerylistr/Fragments/CalendarFragment.java
@@ -35,8 +35,6 @@ public  class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
         return rootView;
     }
 }
