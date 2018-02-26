30a4716ecdc70caccf7be59f7070b0c3308318c2
tk4218 <tk4218@gmail.com>
2018-02-26 00:06:34 -0500

    - Start Login Activity.

---
+++ app/src/main/java/com/tk4218/grocerylistr/NewAccountActivity.java
@@ -0,0 +1,13 @@
+package com.tk4218.grocerylistr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
